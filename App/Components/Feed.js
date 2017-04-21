e1a464143c7c4b7908eca91a5a0bb5a1da01a8be
Christopher King <chrisking1@gmail.com>
2017-04-21 12:35:33 -0400

    fill out constructor in Feed component

--- App/Components/Feed.js
+++ App/Components/Feed.js
@@ -4,6 +4,9 @@ import {
   Text,
   ListView
 } from 'react-native';
+import api from '../Api/api';
+
+let total_feed_items = 1000;
 
 export default class Feed extends Component {
 
@@ -11,7 +14,10 @@ export default class Feed extends Component {
     super();
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
+      title: 'Feed',
       dataSource: ds.cloneWithRows(['woo 1', 'woo 2']),
+      feedItems: {},
+      loaded: false
     };
   }
 
