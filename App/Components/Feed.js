bb2511233f47c7f81dbc9528d2cca4fddc4c7920
Christopher King <chrisking1@gmail.com>
2017-04-21 11:06:50 -0400

    create functioning listview

--- App/Components/Feed.js
+++ App/Components/Feed.js
@@ -5,11 +5,21 @@ import {
 } from 'react-native';
 
 export default class Feed extends Component {
+
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
   render() {
     return (
-      // <ListView>
-        <Text>This is text in Feed.</Text>
-      // </ListView>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
     );
   }
 }
