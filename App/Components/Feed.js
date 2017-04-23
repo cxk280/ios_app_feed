69117069c0abc8ce58e79a1e8b41b129ad796feb
Christopher King <chrisking1@gmail.com>
2017-04-23 15:32:07 -0400

    successfully render ids in listview

--- App/Components/Feed.js
+++ App/Components/Feed.js
@@ -10,23 +10,22 @@ import {
   TouchableHightlight,
   AsyncStorage
 } from 'react-native';
-import Button from 'react-native-button';
-import GiftedSpinner from 'react-native-gifted-spinner';
-import moment from 'moment';
+// import Button from 'react-native-button';
+// import GiftedSpinner from 'react-native-gifted-spinner';
+// import moment from 'moment';
 import api from '../Api/api';
 
-
-const STORAGE_KEY = '@AsyncStorageExample:key';
+let feed_items = [];
 let total_feed_items = 1000;
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 export default class Feed extends Component {
 
   constructor() {
     super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       title: 'Feed',
-      dataSource: ds.cloneWithRows(['']),
+      dataSource: ds.cloneWithRows(feed_items),
       loaded: false
     };
   }
@@ -34,30 +33,28 @@ export default class Feed extends Component {
 
   render() {
       return (
-        <ListView dataSource={this.state.dataSource} renderRow={(rowData) => <Text>{rowData}</Text>}></ListView>
+        <ListView dataSource={this.state.dataSource} renderRow={(data) => <View><Text>{data}</Text></View>}></ListView>
     );
   }
 
 
+  componentWillMount() {
 
-
-  componentDidMount() {
-
-    for(let i = 0; i <= 10; i++){
-      let item_url = "https://api.addicaid.com/feeds?page=" + i;
+    for(let i = 0; i < 10; i++){
+      let item_url = "https://api.addicaid.com/feeds?page=1";
       api(item_url).then(
         (item) => {
-          console.log('item: ',item);
-          feed_items.push(item);
+          console.log('item[0]._id: ',item[0]._id);
+          feed_items.push(item[0]._id);
         }
-      );
-    }
+      ).then(() => {
+        if (i === 9) {
+          console.log('feed_items at for loop 9: ',feed_items);
+          this.setState({dataSource: ds.cloneWithRows(feed_items)});
+        }
+      });
+    };
+    console.log('feed_items after for loop: ',feed_items);
   };
-
-
-
-  viewPage(url){
-    this.props.navigator.push({name: 'web_page', url: url});
-  }
 
 }
--- App/Components/WebPage.js
+++ App/Components/WebPage.js
@@ -7,73 +7,15 @@ import {
   WebView
 } from 'react-native';
 
-import Button from 'react-native-button';
-import GiftedSpinner from 'react-native-gifted-spinner';
+// import Button from 'react-native-button';
+// import GiftedSpinner from 'react-native-gifted-spinner';
 
-import _ from 'lodash';
+// import _ from 'lodash';
 
 export default class WebPage extends Component {
-  getInitialState() {
-    return {
-      isLoading: true
-    };
-  }
-
   render(){
-
     return (
       <Feed />
     );
-
-  }
-
-  truncate(str){
-    return _.truncate(str, 20);
-  }
-
-  onNavigationStateChange(navState) {
-
-    if(!navState.loading){
-      this.setState({
-        isLoading: false,
-        pageTitle: navState.title
-      });
-    }
-  }
-
-  back(){
-     this.props.navigator.pop();
   }
 };
-
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1
-    },
-    webview_header: {
-      paddingLeft: 10,
-      backgroundColor: '#FF6600',
-      flex: 1,
-      justifyContent: 'space-between',
-      flexDirection: 'row'
-    },
-    header_item: {
-      paddingLeft: 10,
-      paddingRight: 10,
-      justifyContent: 'center'
-    },
-    webview_body: {
-      flex: 9
-    },
-    button: {
-      textAlign: 'left',
-      color: '#FFF'
-    },
-    page_title: {
-      color: '#FFF'
-    },
-    spinner: {
-      alignItems: 'flex-end'
-    }
-  });
